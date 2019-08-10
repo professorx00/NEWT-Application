f1ced1c23f69855a1d6758645c7ecddf07caff71
Drew Pepin <drewpepin1995@gmail.com>
2019-08-10 11:21:02 -0400

    Creating foodFact API

--- scripts/FoodFacts.js
+++ scripts/FoodFacts.js
@@ -1,1 +1,24 @@
-//TODO: Facts API
+$(document).ready(function () {
+
+    function getFoodFact() {
+
+        var queryURL = "https://api.spoonacular.com/food/trivia/random?apiKey=615df94260a44b4da806def011b2e2c7"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+
+                let foodFact = response.text;
+                console.log(foodFact);
+
+
+            });
+
+    };
+
+    getFoodFact();
+
+
+});
