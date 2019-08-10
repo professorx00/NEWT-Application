2ad3262d672e375b144c20e4974c909301aad21b
Dave Ward <professorx00@gmail.com>
2019-08-10 12:09:26 -0400

    API is getting products

--- scripts/productSearch.js
+++ scripts/productSearch.js
@@ -1,1 +1,40 @@
 //TODO: Product Search API
+
+const apiKey = `c2bc6078cb1e4d29a1f16dc929782b86`;
+let product='snickers';
+let productIds = [];
+let productData ={}
+
+queryProduct = `https://api.spoonacular.com/food/products/search?query=${product}&number=2&apiKey=${apiKey}`
+
+
+$.get(queryProduct, function () {
+
+}).then(function (apiData) {
+
+    results = apiData.products
+
+    results.forEach(element => {
+        productIds.push(element.id);
+        // console.log(productId)
+    });
+    
+}).then(function(promise){
+
+    productIds.forEach(element=>{
+        let productID = element;
+        queryProdInfo = `https://api.spoonacular.com/food/products/${productID}?${productID}&apiKey=${apiKey}`
+
+        $.get(queryProdInfo, function () {}).then(function(data){
+            productData[data.id] = {
+                id: data.id,
+                title: data.title,
+                ingriendents: data.ingredientList,
+                nutrition:data.nutrition,
+                badges: data.badges
+            }
+        })
+    })
+    console.log(productData)
+});
+
