7feb2e687b17576564777d114476687c05019e15
Drew Pepin <drewpepin1995@gmail.com>
2019-08-10 13:02:05 -0400

    Cleaning up mealPlan API

--- scripts/mealPlan.js
+++ scripts/mealPlan.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
 
     function getMealPlan() {
 
-        let diet = "vegetarian"
-        let timeFrame = "day"
-        let targetCalories = "2000"
-        let exclude = "olives"
+        let diet = ""
+        let timeFrame = ""
+        let targetCalories = ""
+        let exclude = ""
 
         var queryURL = "https://api.spoonacular.com/recipes/mealplans/generate?" + "timeFrame=" + timeFrame + "&targetCalories=" + targetCalories +
         "&diet=" + diet + "&exclude" + exclude + "&apiKey=615df94260a44b4da806def011b2e2c7"
@@ -19,17 +19,6 @@ $(document).ready(function () {
             .then(function (response) {
 
                 console.log(response);
-
-                let mealChoiceOne = response.meals[0];
-                let mealChoiceTwo = response.meals[1];
-                let mealChoiceThree = response.meals[2];
-                let mealNutrients = response.nutrients;
-
-                console.log(mealChoiceOne);
-                console.log(mealChoiceTwo);
-                console.log(mealChoiceThree);
-
-                console.log(mealNutrients);
                 
 
                 
