1f072398f66f0d17fe9f3cff82a4cc9c3e7060f2
Elias Roussos <eliasisaiah@outlook.com>
2019-08-10 13:57:13 -0400

    -got the json from google successfully

--- assets/javascript/eliasApp.js
+++ assets/javascript/eliasApp.js
@@ -12,18 +12,17 @@ $(document).ready(function () {
 
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.09065,-70.80009&radius=1500&type=supermarket&key=AIzaSyDeQ8rVGviT9v6Yqt7Ijg_5bHYFJxni1kU"; // site that doesn’t send Access-Control-*
+
+
+function parsePlaceData(jsonObject){
+    console.log(jsonObject);
+}
+
     fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-        .then(response => response.text())
-        .then(contents => console.log(contents))
+        .then(response => response.json())
+        .then(contents => (console.log(contents.results)))
         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
-    // fetch(`${nearbySearchURL}${googleKey}`)
-    //     .then((locData) => {
-    //         return locData.json();
-    //     })
-    //     .then((jsonData) => {
-    //         console.log(JSON.stringify(jsonData));
-    //     });
 
     if ("geolocation" in navigator) {
         /* geolocation is available */
