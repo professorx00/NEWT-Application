97a27638349dda04b22a6712f8d651fe971c7cfd
Elias Roussos <eliasIsaiah@outlook.com>
2019-08-12 13:18:14 -0400

    -barcode scanning implemented using Quagga.js -starter code courtesy of Kabachnik Andrej

--- assets/javascript/eliasApp.js
+++ assets/javascript/eliasApp.js
@@ -1,84 +1,109 @@
 $(document).ready(function () {
 
-    Quagga.init({
-        inputStream : {
-          name : "Live",
-          type : "LiveStream",
-          target: document.querySelector('#cameraPort')    // Or '#yourElement' (optional)
-        },
-        decoder : {
-          readers : ["code_128_reader"]
+  // Create the QuaggaJS config object for the live stream
+  let liveStreamConfig = {
+    inputStream: {
+      type: "LiveStream",
+      constraints: {
+        width: { min: 640 },
+        height: { min: 480 },
+        aspectRatio: { min: 1, max: 100 },
+        facingMode: "environment" // or "user" for the front camera
+      }
+    },
+    locator: {
+      patchSize: "medium",
+      halfSample: true
+    },
+    numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
+    decoder: {
+      "readers": [
+        { "format": "ean_reader", "config": {} }
+      ]
+    },
+    locate: true
+  };
+  // The fallback to the file API requires a different inputStream option. 
+  // The rest is the same 
+  let fileConfig = $.extend(
+    {},
+    liveStreamConfig,
+    {
+      inputStream: {
+        size: 800
+      }
+    }
+  );
+  
+  // Start the live stream scanner when the modal opens
+  $('#livestream_scanner').on('shown.bs.modal', function (e) {
+    Quagga.init(
+      liveStreamConfig,
+      function (err) {
+        if (err) {
+          $('#livestream_scanner .modal-body .error').html('<div class="alert alert-danger"><strong><i class="fa fa-exclamation-triangle"></i> ' + err.name + '</strong>: ' + err.message + '</div>');
+          Quagga.stop();
+          return;
         }
-      }, function(err) {
-          if (err) {
-              console.log(err);
-              return
-          }
-          console.log("Initialization finished. Ready to start");
-          Quagga.start();
-      });
-
-    // Quagga.decodeSingle({
-    //     decoder: {
-    //         readers: ["code_128_reader"] // List of active readers
-    //     },
-    //     locate: true, // try to locate the barcode in the image
-    //     src: '/test/fixtures/code_128/image-001.jpg' // or 'data:image/jpg;base64,' + data
-    // }, function (result) {
-    //     if (result.codeResult) {
-    //         console.log("result", result.codeResult.code);
-    //     } else {
-    //         console.log("not detected");
-    //     }
-    // });
+        Quagga.start();
+      }
+    );
+  });
 
 
-    // const googleKey = "AIzaSyDeQ8rVGviT9v6Yqt7Ijg_5bHYFJxni1kU";
 
-    // let findPlaceURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters";
-    // let radius = 0;
-    // let currentLat = null;
-    // let currentLong = null;
-    // let nearbySearchURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.09065,-70.80009&radius=1500&type=supermarket&key=`
+  // Make sure, QuaggaJS draws frames an lines around possible 
+  // barcodes on the live stream
+  Quagga.onProcessed(function (result) {
+    let drawingCtx = Quagga.canvas.ctx.overlay,
+      drawingCanvas = Quagga.canvas.dom.overlay;
 
-    // let openMapURL = "https://www.openstreetmap.org/export/embed.html?bbox=-2.493209838867188%2C53.50540525319918%2C-2.246360778808594%2C53.61980121473449&amp;layer=mapnik&amp;marker=53.56274386269267%2C-2.3699569702148438"
+    if (result) {
+      if (result.boxes) {
+        drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+        result.boxes.filter(function (box) {
+          return box !== result.box;
+        }).forEach(function (box) {
+          Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+        });
+      }
 
+      if (result.box) {
+        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+      }
 
-    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    // const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.09065,-70.80009&radius=1500&type=supermarket&key=AIzaSyDeQ8rVGviT9v6Yqt7Ijg_5bHYFJxni1kU"; // site that doesn’t send Access-Control-*
+      if (result.codeResult && result.codeResult.code) {
+        Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+      }
+    }
+  });
 
-    // //array variable declaration
-    // let lats = null;
-    // let longs = null;
+  // Once a barcode had been read successfully, stop quagga and 
+  // close the modal after a second to let the user notice where 
+  // the barcode had actually been found.
+  
+  Quagga.onDetected(function (result) {
+    console.log("reached onDetected");
+    // alert(result.codeResult.code);
+    if (result.codeResult.code) {
+      $('#scanner_input').val(result.codeResult.code);
+      Quagga.stop();
+      setTimeout(function () { $('#livestream_scanner').modal('hide'); }, 1000);
+    }
+  });
 
+  // Stop quagga in any case, when the modal is closed
+  $('#livestream_scanner').on('hidden.bs.modal', function () {
+    if (Quagga) {
+      Quagga.stop();
+    }
+  });
 
-    // function getPlaceData(jsonObject){
-    //     jsonObject.forEach(element => {
-    //         lats.push(element.geometry.location.lat);
-    //         longs.push(elment.geometry.location.lng);
-    //     });
-    //     console.log("lats, longs:\n---------------------------");
-    //     console.log(lats);
-    //     console.log(longs);
-    // }
-
-    //     fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-    //         .then(response => response.json())
-    //         .then(contents => {
-    //             console.log(getPlaceData(contents.results))
-    //         })
-    //         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-
-
-    //     if ("geolocation" in navigator) {
-    //         /* geolocation is available */
-    //         navigator.geolocation.getCurrentPosition(function (position) {
-    //             currentLat = position.coords.latitude;
-    //             console.log(`current latitude: ${currentLat}`);
-    //             currentLong = position.coords.longitude;
-    //             console.log(`current longitude: ${currentLong}`);
-    //         });
-    //     } else {
-    //         console.log("geolocation IS NOT available");
-    //     }
+  // Call Quagga.decodeSingle() for every file selected in the 
+  // file input
+  $("#livestream_scanner input:file").on("change", function (e) {
+    if (e.target.files && e.target.files.length) {
+      Quagga.decodeSingle($.extend({}, fileConfig, { src: URL.createObjectURL(e.target.files[0]) }), function (result) { alert(result.codeResult.code); });
+    }
+  });
 })
