40c615eca83da65b49e7799d3aaf6e3516c66353
Dave Ward <professorx00@gmail.com>
2019-08-12 15:43:15 -0400

    Product Search is working except it's not doing anything with calories

--- scripts/productSearch.js
+++ scripts/productSearch.js
@@ -1,40 +1,76 @@
 //TODO: Product Search API
+//TODO: Grab Fields from Index
+//TODO: Pushback data
 
 const apiKey = `c2bc6078cb1e4d29a1f16dc929782b86`;
-let product='snickers';
+const apiKey2 = '9b0c0f062fd744b29e02ffbea812b474 ';
+
 let productIds = [];
 let productData ={}
 
-queryProduct = `https://api.spoonacular.com/food/products/search?query=${product}&number=2&apiKey=${apiKey}`
+//document Jquery Items:
+
+const product = $("#query");
+const btn = $("#productSearchBtn");
+const glist =$("#gList")
+const results=$("#prodSearchResults")
 
 
-$.get(queryProduct, function () {
 
-}).then(function (apiData) {
 
-    results = apiData.products
+btn.on("click",(e)=>{
+    console.log("clicked")
+    e.preventDefault();
+    item=product.val()
+    console.log(item)
+    queryProduct = `https://api.spoonacular.com/food/products/search?query=${item}&number=2&apiKey=${apiKey}`
 
-    results.forEach(element => {
-        productIds.push(element.id);
-        // console.log(productId)
-    });
+
+    $.get(queryProduct, function () {
+
+    }).then(function (apiData) {
     
-}).then(function(promise){
+        apiResults = apiData.products
+        console.log(apiResults)
+    
+        apiResults.forEach(element => {
+            productIds.push(element.id);
+            // console.log(productId)
+        });
+        console.log(productIds)
+    }).then(function(promise){
+    
+        productIds.forEach(element=>{
+            let productID = element;
+            queryProdInfo = `https://api.spoonacular.com/food/products/${productID}?${productID}&apiKey=${apiKey2}`
+    
+            $.get(queryProdInfo, function () {}).then(function(data){
+                console.log(results)
+                docNewDiv = $("<div>").attr("data-id", data.id).append($("<h1>").text(data.title)).append($("<p>").text(data.ingredientList))
+                console.log(docNewDiv)
+                docNutrition = $("<ul>")
+                console.log(data.nutrition.calories)
+                docCalories = $("<li>").text(`Calories: ${data.nutrition.calories}`)
+                docCarbs = $("<li>").text(`Carbs: ${data.nutrition.carbs}`)
+                docFat = $("<li>").text(`Fat: ${data.nutrition.fat}`)
+                docProtein = $("<li>").text(`Protein: ${data.nutrition.protein}`)
 
-    productIds.forEach(element=>{
-        let productID = element;
-        queryProdInfo = `https://api.spoonacular.com/food/products/${productID}?${productID}&apiKey=${apiKey}`
+                docNutrition.append(docCalories,docCarbs,docFat,docProtein)
+                console.log(docNutrition)
+                docNewDiv.append(docNutrition)
 
-        $.get(queryProdInfo, function () {}).then(function(data){
-            productData[data.id] = {
-                id: data.id,
-                title: data.title,
-                ingriendents: data.ingredientList,
-                nutrition:data.nutrition,
-                badges: data.badges
-            }
+                results.append(docNewDiv)
+
+                productData[data.id] = {
+                    id: data.id,
+                    title: data.title,
+                    ingriendents: data.ingredientList,
+                    nutrition:data.nutrition,
+                    badges: data.badges
+                }
+            })
         })
+    }).then(()=>{
+        console.log(productData)
     })
-    console.log(productData)
 });
-
