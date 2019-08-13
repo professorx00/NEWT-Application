0757b9b3cbd4c5b4fca859ea6641db8b12dfd351
Drew Pepin <drewpepin1995@gmail.com>
2019-08-12 20:29:28 -0400

    Pulling data from mealPlanWeek API

--- scripts/mealPlan.js
+++ scripts/mealPlan.js
@@ -94,8 +94,65 @@ $(document).ready(function () {
         })
             .then(function (response) {
 
-
                 console.log(response);
+
+                let MealPlanTitle = response.name;
+
+                let dayOneMealOneTitle = JSON.parse(response.items[0].value).title;
+                let dayOneMealOneId = JSON.parse(response.items[0].value).id;
+                let dayOneMealTwoTitle = JSON.parse(response.items[1].value).title;
+                let dayOneMealTwoId = JSON.parse(response.items[1].value).id;
+                let dayOneMealThreeTitle = JSON.parse(response.items[2].value).title;
+                let dayOneMealThreeId = JSON.parse(response.items[2].value).id;
+
+                let dayTwoMealOneTitle = JSON.parse(response.items[3].value).title;
+                let dayTwoMealOneId = JSON.parse(response.items[3].value).id;
+                let dayTwoMealTwoTitle = JSON.parse(response.items[4].value).title;
+                let dayTwoMealTwoId = JSON.parse(response.items[4].value).id;
+                let dayTwoMealThreeTitle = JSON.parse(response.items[5].value).title;
+                let dayTwoMealThreeId = JSON.parse(response.items[5].value).id;
+
+                let dayThreeMealOneTitle = JSON.parse(response.items[6].value).title;
+                let dayThreeMealOneId = JSON.parse(response.items[6].value).id;
+                let dayThreeMealTwoTitle = JSON.parse(response.items[7].value).title;
+                let dayThreeMealTwoId = JSON.parse(response.items[7].value).id;
+                let dayThreeMealThreeTitle = JSON.parse(response.items[8].value).title;
+                let dayThreeMealThreeId = JSON.parse(response.items[8].value).id;
+
+                let dayFourMealOneTitle = JSON.parse(response.items[9].value).title;
+                let dayFourMealOneId = JSON.parse(response.items[9].value).id;
+                let dayFourMealTwoTitle = JSON.parse(response.items[10].value).title;
+                let dayFourMealTwoId = JSON.parse(response.items[10].value).id;
+                let dayFourMealThreeTitle = JSON.parse(response.items[11].value).title;
+                let dayFourMealThreeId = JSON.parse(response.items[11].value).id;
+
+                let dayFiveMealOneTitle = JSON.parse(response.items[12].value).title;
+                let dayFiveMealOneId = JSON.parse(response.items[12].value).id;
+                let dayFiveMealTwoTitle = JSON.parse(response.items[13].value).title;
+                let dayFiveMealTwoId = JSON.parse(response.items[13].value).id;
+                let dayFiveMealThreeTitle = JSON.parse(response.items[14].value).title;
+                let dayFiveMealThreeId = JSON.parse(response.items[14].value).id;
+
+                let daySixMealOneTitle = JSON.parse(response.items[15].value).title;
+                let daySixMealOneId = JSON.parse(response.items[15].value).id;
+                let daySixMealTwoTitle = JSON.parse(response.items[16].value).title;
+                let daySixMealTwoId = JSON.parse(response.items[16].value).id;
+                let daySixMealThreeTitle = JSON.parse(response.items[17].value).title;
+                let daySixMealThreeId = JSON.parse(response.items[17].value).id;
+
+                let daySevenMealOneTitle = JSON.parse(response.items[18].value).title;
+                let daySevenMealOneId = JSON.parse(response.items[18].value).id;
+                let daySevenMealTwoTitle = JSON.parse(response.items[19].value).title;
+                let daySevenMealTwoId = JSON.parse(response.items[19].value).id;
+                let daySevenMealThreeTitle = JSON.parse(response.items[20].value).title;
+                let daySevenMealThreeId = JSON.parse(response.items[20].value).id;
+
+
+                
+                
+
+                
+                
 
 
 
