877f7970d932f74abc4d5450570dfe8a5949412e
Dave Ward <professorx00@gmail.com>
2019-08-14 19:39:37 -0400

    Got Food Facts Working

--- scripts/FoodFacts.js
+++ scripts/FoodFacts.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-    imgUrls = [food1,food2,food3,food4,food5,food6,food7,food8,food9,food10,food11,food12,food13,food14,food15,food16,food17,food18]
+    imgUrls = ["food1","food2","food3","food4","food5","food6","food7","food8","food9","food10","food11","food12","food13","food14","food15","food16","food17","food18"]
 
-    console.log("fun")
      let apiKey = "e9a27e4da700489a836bf1b7cdc449b9"
     //615df94260a44b4da806def011b2e2c7
     function getFoodFact(i,img) {
@@ -17,7 +16,7 @@ $(document).ready(function () {
                 let foodFact = response.text;
                 console.log(foodFact)
                 let foodcard = $("<div>").addClass("card")
-                let imgTag = $("<img>").attr("src", `./imgs/${img}.jpg`)
+                let imgTag = $("<img>").attr("src", `./images/food/${img}.jpg`).addClass("foodImg")
                 let foodFactCard = $("<div>").addClass("card-body")
                 let foodTitle = $("<H5>").addClass("card-title").text(`Food Fact # ${i+1}`)
                 let foodBody = $("<p>").addClass("card-text").text(foodFact)
@@ -28,13 +27,11 @@ $(document).ready(function () {
             });
 
     };
-
-    for (i = 0; i < 9; i++) {
-        num = Math.floor(Math.random*8)
-        for(j=num; j<num+9; j++){
-            getFoodFact(i,imgUrls[j]);
-        }
-        
+    num = Math.floor(Math.random()*8)
+    console.log(num)
+    for (i = num; i < num+9; i++) {
+        console.log("images")
+        getFoodFact(i,imgUrls[i]);
     };
 
 
