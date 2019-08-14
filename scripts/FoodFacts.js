5092e01d74c60c801d0ff658c194ac246653ef20
Dave Ward <professorx00@gmail.com>
2019-08-14 19:26:51 -0400

    Save to my branch

--- scripts/FoodFacts.js
+++ scripts/FoodFacts.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
+    imgUrls = [food1,food2,food3,food4,food5,food6,food7,food8,food9,food10,food11,food12,food13,food14,food15,food16,food17,food18]
 
-    function getFoodFact() {
+    console.log("fun")
+     let apiKey = "e9a27e4da700489a836bf1b7cdc449b9"
+    //615df94260a44b4da806def011b2e2c7
+    function getFoodFact(i,img) {
 
-        var queryURL = "https://api.spoonacular.com/food/trivia/random?apiKey=615df94260a44b4da806def011b2e2c7"
+        let queryURL = `https://api.spoonacular.com/food/trivia/random?apiKey=${apiKey}`;
 
         $.ajax({
             url: queryURL,
@@ -11,26 +15,26 @@ $(document).ready(function () {
             .then(function (response) {
 
                 let foodFact = response.text;
-                
-                let foodFactDiv = $("<div>")
-                
-                let p = $("<p>").text("Food Fact : " + foodFact);
-
-                foodFactDiv.append(p);
-
-
-                $("#foodFactDiv").append(foodFactDiv);
-                
-
-                
-
+                console.log(foodFact)
+                let foodcard = $("<div>").addClass("card")
+                let imgTag = $("<img>").attr("src", `./imgs/${img}.jpg`)
+                let foodFactCard = $("<div>").addClass("card-body")
+                let foodTitle = $("<H5>").addClass("card-title").text(`Food Fact # ${i+1}`)
+                let foodBody = $("<p>").addClass("card-text").text(foodFact)
+                foodFactCard.append(foodTitle,foodBody)
+                foodcard.append(imgTag,foodFactCard).attr("id", "foodSection")
+                $("#factContainer").append(foodcard)
 
             });
 
     };
 
-    for (i = 0; i < 10; i++) {
-        getFoodFact(i);
+    for (i = 0; i < 9; i++) {
+        num = Math.floor(Math.random*8)
+        for(j=num; j<num+9; j++){
+            getFoodFact(i,imgUrls[j]);
+        }
+        
     };
 
 
--- scripts/productSearch.js
+++ scripts/productSearch.js
@@ -1,119 +1,25 @@
-//TODO: Product Search API
-//TODO: Grab Fields from Index
-//TODO: Pushback data
 
-const apiKey = `c2bc6078cb1e4d29a1f16dc929782b86`;
-const apiKey2 = '9b0c0f062fd744b29e02ffbea812b474 ';
-
-let productIds = [];
-let productData = {}
-
-
-const product = $("#query");
-const btn = $("#productSearchBtn");
-const glist = $("#gList")
-const results = $("#prodSearchResults")
-const pForm = $("#productForm");
-const minCal = $("#minCalories");
-const maxCal = $("#maxCalories");
-
-
-btn.on("click", (e) => {
-    results.empty();
-    productIds = [];
-    console.log("clicked")
-    e.preventDefault();
-    item = product.val()
-    minCalories = minCal.val().trim();
-    maxCalories = maxCal.val().trim();
-
-    if(minCalories && !maxCalories){
-        queryProduct = `https://api.spoonacular.com/food/products/search?query=${item}&number=2&minCalories=${minCalories}&apiKey=${apiKey}`
-    }
-    else if(!minCalories && maxCalories){
-        queryProduct = `https://api.spoonacular.com/food/products/search?query=${item}&number=2&maxCalories=${maxCalories}&apiKey=${apiKey}`
-    }
-    else if(minCalories && maxCalories){
-        queryProduct = `https://api.spoonacular.com/food/products/search?query=${item}&number=2&maxCalories=${maxCalories}&minCalories=${minCalories}&apiKey=${apiKey}`
-    }
-    else{
-        queryProduct = `https://api.spoonacular.com/food/products/search?query=${item}&number=2&apiKey=${apiKey}`
-    }
-
-    console.log(queryProduct)    
-
-
-    $.get(queryProduct, function () {
-
-    }).then(function (apiData) {
-
-        apiResults = apiData.products
-
-        apiResults.forEach(element => {
-            productIds.push(element.id);
-            // console.log(productId)
-        });
-    }).then(function (promise) {
-
-        productIds.forEach(element => {
-            let productID = element;
-            queryProdInfo = `https://api.spoonacular.com/food/products/${productID}?${productID}&apiKey=${apiKey2}`
-
-            $.get(queryProdInfo, function () { }).then(function (data) {
-                // new Product Div with title,and ingrident
-                docNewDiv = $("<div>").attr("data-id", data.id).attr("id", data.id).append($("<h1>").text(data.title)).append($("<p>").text(data.ingredientList))
-                //nutrition List
-                docProductBtn = $("<button>").addClass("productBtn btn btn-dark").text("Add").attr("data-title", data.title).attr("data-id", data.id)
-                console.log(docProductBtn)
-                docNutrition = $("<ul>")
-
-                docCalories = $("<li>").text(`Calories: ${data.nutrition.calories}`)
-                docCarbs = $("<li>").text(`Carbs: ${data.nutrition.carbs}`)
-                docFat = $("<li>").text(`Fat: ${data.nutrition.fat}`)
-                docProtein = $("<li>").text(`Protein: ${data.nutrition.protein}`)
-                // appending list to nutriention list
-                docNutrition.append(docCalories, docCarbs, docFat, docProtein)
-                //Appending  nurtiention list to div
-                docNewDiv.append(docNutrition)
-                docNewDiv.append(docProductBtn)
-                //append to page
-                results.append(docNewDiv)
-
-                productData[data.id] = {
-                    id: data.id,
-                    title: data.title,
-                    ingriendents: data.ingredientList,
-                    nutrition: data.nutrition,
-                    badges: data.badges
-                }
-            })
             
-        })
-    }).then(() => {
+            <div class="col-lg-9" id="foodFacts">
+                <div style="background-color: #DCDCDC; color: black; height: 100%">
+                    <div class="container">
+                        <h1 class="text-center"> Food Facts </h1>
+                        <br>
+                        <div class="card-deck" id="factContainer">
+                       
+                    </div>
+                </div>
+            </div>
 
-    })
-});
-
-addClick = function (event) {
 
 
 
-    let title = $(event.target).attr("data-title");
-    let id = $(event.target).attr("data-id");
-
-    const newItem = $("<li>").attr("id", `${id}List`).append($("<button>").addClass("btn btn-dark listItem").attr("data-id", id).text(title));
-    glist.append(newItem)
-    $(`#${id}`).remove();
-
-}
-
-listItemBtn = function (event) {
-    console.log("clicked List Item")
-    id = $(event.target).attr("data-id")
-    $(`#${id}List`).remove();
-}
-
-$(document).on("click", "button.productBtn", addClick);
-
-$(document).on("click", "button.listItem", listItemBtn);
-
+            #factContainer{
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr;
+                margin: 2rem;
+            }
+            
+            #foodSection{
+                margin: 1rem;
+            }
